602548448dea666c10b87f6514630379d719e3f7
Maxim <mystic189123@gmail.com>
2024-09-04 10:40:30 +0300

    Kaikki Tehtävät

---
+++ hello-world-My-stuff/README.md
@@ -0,0 +1,21 @@
+# About me:
+
+Hi I'm Maxim, I'm 16 years old and I love to code. I love to hangout with friends and my family. I'm in love with video games and love to talk to random people :D!
+
+# Table:
+| Fruits  | Vegetables |
+| ------------- | ------------- |
+| Apple  | Carrot  |
+| Watermelon  | Tomato  |
+
+# List:
+
+```
+1. Eat
+2. Sleep
+3. Game
+4. REEAT
+```
+
+![image](https://github.com/user-attachments/assets/75ca929b-34c3-43e9-aef5-7a76beb3710e)
+
---
+++ hello-world-My-stuff/main.js
@@ -0,0 +1,60 @@
+function ChangeColor(){
+    document.getElementById("btn").style.color = "red"
+    document.getElementById("btn").innerHTML = "I can change Color!"
+}
+
+function ChangeColor2(){
+    document.getElementById("btn2").innerHTML = "I can change text and bg!"
+    document.getElementById("btn2").style.background = "green"
+}
+
+function Age(){
+    let age = document.getElementById("age").value;
+    let voteable
+    if (age < 18){
+        voteable = "Too young"
+        document.getElementById("test").innerHTML = voteable + " to drink beer.";
+    } else if (age >= 18) {
+        voteable = "Old enough"
+        document.getElementById("test").innerHTML = voteable + " to drink beer.";
+    } else {
+        voteable = "That is not a number dumbass!"
+        document.getElementById("test").innerHTML = voteable;
+    }
+}
+
+function OnCLick(){
+    let text = "";
+    let i = 1;
+    
+    do {
+        text += "<br>Huuhuu " + i;
+        i++;
+    }
+
+    while (i < 6) {
+        document.getElementById("Huuhuu").innerHTML = text;
+    }
+}
+
+function ChangeList(){
+    let text = "";
+    let i = 0;
+    const myList = ["Koira","Kissa","Hevonen", "Hiiri","Marsu"]
+
+    do {
+        text += "<br>" + myList[i] + "!";
+        i++;
+    }
+
+    while (i < 5) {
+        document.getElementById("BetterList").innerHTML = text;
+    }
+}
+
+function fullName(){
+    let name = document.getElementById("name").value
+    let lname = document.getElementById("lname").value
+    var text = name + " " + lname
+    document.getElementById("information").innerHTML = "Hei: " + text
+}
